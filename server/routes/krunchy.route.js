8649fb3860cd095e8be30c969d9b24b880649bf0
yacine <yacineaaddi@gmail.com>
2026-03-19 16:52:52 +0000

    Update .env Urls

--- server/routes/krunchy.route.js
+++ server/routes/krunchy.route.js
@@ -115,25 +115,22 @@ router.post("/login/driver", async (req, res) => {
     user.refreshToken = refreshToken;
     await user.save();
 
-    console.log(accessToken);
-    console.log(refreshToken);
-
     res
       .cookie("accessToken", accessToken, {
-        secure: false, // important for localhost
-        sameSite: "lax", // works with localhost ports
+        //secure: false, // important for localhost
+        //sameSite: "lax", // works with localhost ports
         httpOnly: true,
         path: "/",
-        //secure: true,
-        //sameSite: "none",
+        secure: true,
+        sameSite: "none",
       })
       .cookie("refreshToken", refreshToken, {
-        secure: false, // important for localhost
-        sameSite: "lax", // works with localhost ports
+        //secure: false, // important for localhost
+        //sameSite: "lax", // works with localhost ports
         httpOnly: true,
         path: "/refresh",
-        //secure: true,
-        //sameSite: "none",
+        secure: true,
+        sameSite: "none",
       })
       .status(200)
       .json({
@@ -170,20 +167,20 @@ router.post("/login/admin", async (req, res) => {
 
     res
       .cookie("accessToken", accessToken, {
-        secure: false, // important for localhost
-        sameSite: "lax", // works with localhost ports
+        //secure: false, // important for localhost
+        //sameSite: "lax", // works with localhost ports
         httpOnly: true,
         path: "/",
-        //secure: true,
-        //sameSite: "none",
+        secure: true,
+        sameSite: "none",
       })
       .cookie("refreshToken", refreshToken, {
-        secure: false, // important for localhost
-        sameSite: "lax", // works with localhost ports
+        //secure: false, // important for localhost
+        //sameSite: "lax", // works with localhost ports
         httpOnly: true,
         path: "/refresh",
-        //secure: true,
-        //sameSite: "none",
+        secure: true,
+        sameSite: "none",
       })
       .status(200)
       .json({
